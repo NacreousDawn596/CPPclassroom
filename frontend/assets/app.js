1b4c9d88a986a9cfb78d1d85b7964365f2237f01
NacreousDawn596 <kamal@aferiad.xyz>
2025-12-02 00:31:53 +0100

    migrated from clouflare containers to fly.io

--- frontend/assets/app.js
+++ frontend/assets/app.js
@@ -1,7 +1,7 @@
 const IS_LOCAL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 const API_URL = IS_LOCAL
-    ? 'http://localhost:8787/api'
-    : 'https://cppclassroombackend.k-aferiad.workers.dev/api'; // Relative path for Cloudflare Pages + Workers integration
+    ? 'http://localhost:5550'
+    : 'https://backend-snowy-wildflower-8765.fly.dev';
 const STORAGE_KEY = 'cpp_playground_files';
 
 let editor;
